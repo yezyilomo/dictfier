94ebc6012926e222d7147feda1015aaa8a27f94e
yezy <yezileliilomo@hotmail.com>
2019-03-18 16:39:54 +0300

    Add dictifier library

---
+++ dictifier/api.py
@@ -0,0 +1,9 @@
+from . import factory
+
+def dictify(obj, query, call_callable=False, not_found_create=False):
+    return factory._dict(
+        obj, 
+        query, 
+        call_callable, 
+        not_found_create
+    )

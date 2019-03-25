a3495ba7e51b3d830dd0fb30d9da7557a308a748
yezy <yezileliilomo@hotmail.com>
2019-03-25 15:59:36 +0300

    Add new API and Fix some bugs
    -Add newfield API
    -Add serializer kwarg to dictfy API
    -Fix(Raising FormatError instead of TypeError)
    -Improve code readability

--- dictfier/factory.py
+++ dictfier/factory.py
@@ -1,8 +1,16 @@
+import json
 from .exceptions import FormatError
+
 
 class UseObj(object):
     def __init__(self, function):
         self.function = function
+
+
+class NewField(object):
+    def __init__(self, value):
+        self.value = value
+
 
 def valid_query(obj, query):
     flat_or_nested = all(
@@ -25,7 +33,15 @@ def valid_query(obj, query):
         return False
 
 
-def _dict(obj, query, call_callable, not_found_create, fields=None):
+def is_serealizable(data):
+    try:
+        json.dumps(data)
+        return True
+    except:
+        return False
+
+
+def _dict(obj, query, call_callable, serializer, fields=None):
     # Check if the query node is valid against object
     if not valid_query(obj, query):
         message = "Invalid Query format on \"%s\" node." % str(query)
@@ -37,77 +53,104 @@ def _dict(obj, query, call_callable, not_found_create, fields=None):
             if fields is None:
                      fields = {}
             if callable(getattr(obj, field)) and call_callable:
-                fields.update({field: getattr(obj, field)()})
+                field_value = getattr(obj, field)()
             else:
-                fields.update({field: getattr(obj, field)})
+                field_value = getattr(obj, field)
+            
+            if serializer is not None and not is_serealizable(field_value):
+                fields.update({field: serializer( field_value )})
+            else:
+                fields.update({field: field_value})
 
         elif isinstance(field, dict):
-            # Nested field or new field
+            # Nested or new field
             if fields is None:
                      fields = {}
                 
             for sub_field in field:
-                found = hasattr(obj, sub_field)
-
-                if not_found_create and not found:
-                    # Create new field
-                    if isinstance(field[sub_field], UseObj) and callable(field[sub_field].function(obj)) and call_callable:
-                        fields.update({sub_field: field[sub_field].function(obj)()})
-                    elif isinstance(field[sub_field], UseObj) and callable(field[sub_field].function(obj)):
-                        fields.update({sub_field: field[sub_field].function(obj)})
-                    else:
-                        fields.update({sub_field: field[sub_field]})
+                if (isinstance(field[sub_field], NewField) and 
+                        callable(field[sub_field].value) and 
+                        call_callable):
+                    field_value = field[sub_field].value()
+                    fields.update({sub_field: field_value})
                     continue
-                elif not found:
-                    # Throw NotFound Error [FIXME]
-                    getattr(obj, sub_field)
+                elif isinstance(field[sub_field], NewField):
+                    field_value = field[sub_field].value
+                    fields.update({sub_field: field_value})
                     continue
-
-                if len(field[sub_field]) < 1:
+                elif (isinstance(field[sub_field], UseObj) and 
+                        callable(field[sub_field].function(obj)) and 
+                        call_callable):
+                    sub_field_value = field[sub_field].function(obj)()
+                    fields.update({sub_field: sub_field_value})
+                    continue
+                elif isinstance(field[sub_field], UseObj):
+                    sub_field_value = field[sub_field].function(obj)
+                    fields.update({sub_field: sub_field_value})
+                    continue
+                elif (isinstance(field[sub_field], (list, tuple)) and 
+                        len(field[sub_field]) < 1):
                     # Nested empty object,
                     # Empty dict is the default value for empty nested objects.
                     # Comment the line below to remove empty objects in results. [FIXME]
                     fields.update({sub_field: {}})
                     continue
-
-                if isinstance(field[sub_field][0], (list, tuple)):
-                    # Nested object is iterable
-                    fields.update({sub_field: []})
+                elif (isinstance(field[sub_field], (list, tuple)) and 
+                        len(field[sub_field]) == 1 and 
+                        isinstance(field[sub_field][0], (list, tuple))):
+                    # Nested object is iterable 
+                    fields.update({sub_field: []})  # Then call _dict again
+                elif (isinstance(field[sub_field], (list, tuple)) and 
+                        len(field[sub_field]) > 0):
+                    # Nested object is flat 
+                    fields.update({sub_field: {}})  # Then call _dict again
                 else:
-                    # Nested object is flat
-                    fields.update({sub_field: {}})
+                    # Ivalid Assignment of value to a field
+                    message = (
+                        "'%s' value must be of type "
+                        "NewField or UseObj, not '%s'. "
+                        "Refer to 'useobj', 'usefield' or 'newfield' "
+                        "APIs for more details "
+                    ) % (str(sub_field), type(field[sub_field]).__name__)
+                    raise TypeError(message)
 
                 obj_field = getattr(obj, sub_field)
                 if callable(obj_field) and call_callable:
                     obj_field = obj_field()
 
+                # This will be executed if Nested object is flat or iterable
+                # since they are the only conditions without continue statement
                 _dict(
                     obj_field,
                     field[sub_field],
                     call_callable,
-                    not_found_create,
+                    serializer,
                     fields=fields[sub_field],
                 )
 
         elif isinstance(field, (list, tuple)):
-            # Nested object
+            # Iterable nested object
             if fields is None:
                      fields = []
             for sub_obj in obj:
                 sub_field = {}
                 fields.append(sub_field)
+
+                # dictfy all objects in iterable object
                 _dict(
                     sub_obj,
                     field,
                     call_callable,
-                    not_found_create,
+                    serializer,
                     fields=sub_field
                 )
         else:
-            message = """
-                Wrong formating of Query on '%s' node, It seems like the Query was mutated on run time.
-                Use 'tuple' instead of 'list' to avoid mutating Query accidentally.
-                """ % str(field)
+            message = (
+                "Wrong formating of Query on '%s' node, "
+                "It seems like the Query was mutated during run time."
+                "Use 'tuple' instead of 'list' to avoid mutating Query "
+                "accidentally."
+            ) % str(field)
             raise FormatError(message)
 
     return fields

095a6a4d29ce110a031d80e9b3fa70ac5f63700a
yezy <yezileliilomo@hotmail.com>
2019-04-05 16:02:39 +0300

    API changes
    -Deprecate call_callable and serializer kwargs on dictfy API
    -Add call kwarg on usefield API

--- dictfier/api.py
+++ dictfier/api.py
@@ -1,18 +1,25 @@
 from . import factory
 
-def dictfy(obj, query, call_callable=False, serializer=None):
+def dictfy(obj, query):
     return factory._dict(
         obj, 
         query, 
-        call_callable, 
-        serializer,
     )
 
-def useobj(function):
-    return factory.UseObj(function)
 
-def usefield(field_name):
-    return useobj(lambda obj: getattr(obj, field_name))
+def useobj(function, fields=None):
+    return factory.UseObj(function, fields)
+
+
+def usefield(field_name, call=False, args=tuple(), kwargs={}):
+    if call:
+        return useobj(
+            lambda obj: getattr(obj, field_name)(*args, **kwargs), 
+            fields=None
+        )
+    else:
+        return useobj(lambda obj: getattr(obj, field_name), fields=None)
+
 
 def newfield(value):
     return factory.NewField(value)
